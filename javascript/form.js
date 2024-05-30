71ca7b1d6b95e4f945208e63dd2d5317630cd983
Josselin <josselin.keib@efrei.net>
2024-05-30 10:13:46 +0200

    Add form

---
+++ javascript/form.js
@@ -0,0 +1,12 @@
+
+function submitForm() {
+    event.preventDefault();
+
+    alert("Your feedback has been submitted. Thank you!");
+
+    document.getElementById('Helpform').reset();
+
+
+    return false; 
+}
+
